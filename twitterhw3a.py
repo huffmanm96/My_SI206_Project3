1386d52f02e034385a77bd820078749c508b0636
Madison <huffmanm96@gmail.com>
2016-11-10 11:04:19 -0500

    Commit 9: Adding finished twitter code for part a.

--- twitterhw3a.py
+++ twitterhw3a.py
@@ -3,6 +3,19 @@
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
+import tweepy
+import tokens
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+# Boilerplate code here
+auth = tweepy.OAuthHandler(tokens.consumer_key,tokens.consumer_secret)
+auth.set_access_token(tokens.access_token,tokens.access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+try:
+    api.update_with_media('umsi_internal_yellow_M2.jpg',"#UMSI206 #Proj3")
+    print ("Success!")
+except:
+    print ("Fail... womp.")
+
+
