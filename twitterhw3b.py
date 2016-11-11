9be3f557c398bf964eec6066db29962683715061
Madison <huffmanm96@gmail.com>
2016-11-11 15:24:45 -0500

    Commit 10: Added finished twitter code for part b, as well as the .jpg file for the picture to be tweeted in the first twitter problem, part a.

--- twitterhw3b.py
+++ twitterhw3b.py
@@ -7,6 +7,37 @@
 
 # Be prepared to change the search term during demo.
 
+import tweepy
+from textblob import TextBlob
+import tokens
 
-print("Average subjectivity is")
-print("Average polarity is")
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(tokens.consumer_key,tokens.consumer_secret)
+auth.set_access_token(tokens.access_token,tokens.access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+polarity = 0
+subjectivity = 0
+count = 0
+print ('\n')
+print ("~~~~~~~~ TWEETS ~~~~~~~~")
+print ('\n')
+public_tweets = api.search('ElectionNight')
+for tweet in public_tweets:
+     print(tweet.text)
+     count += 1
+     analysis = TextBlob(tweet.text)
+     print (analysis.sentiment)
+     polarity += analysis.sentiment.polarity
+     subjectivity += analysis.sentiment.subjectivity
+     print ('\n')
+     
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Average subjectivity is " + str(subjectivity/count))
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Average polarity is " + str(polarity/count))
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print ('\n')
